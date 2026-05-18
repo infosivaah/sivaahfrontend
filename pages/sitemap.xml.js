1323d2fb95c392774210027d1fd8fe3e71e4b9b1
mk2429 <mayank.20429@knit.ac.in>
2026-05-18 19:46:59 +0530

    new we6

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,109 @@
+const BASE_URL =
+  "https://www.sivaah.in";
+
+function generateSiteMap({
+  products,
+  categories
+}) {
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+
+  <urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  >
+
+    <!-- STATIC -->
+
+    <url>
+      <loc>${BASE_URL}</loc>
+    </url>
+
+    <url>
+      <loc>${BASE_URL}/shop</loc>
+    </url>
+
+    <!-- CATEGORY PAGES -->
+
+    ${categories.map(category => `
+
+      <url>
+
+        <loc>
+
+          ${BASE_URL}/collections/${category.name}
+
+        </loc>
+
+      </url>
+
+    `).join("")}
+
+    <!-- PRODUCTS -->
+
+    ${products.map(product => `
+
+      <url>
+
+        <loc>
+
+          ${BASE_URL}/product/${product.slug}
+
+        </loc>
+
+      </url>
+
+    `).join("")}
+
+  </urlset>
+  `;
+}
+
+export async function getServerSideProps({
+  res
+}) {
+
+  const [
+    productsRes,
+    categoriesRes
+  ] = await Promise.all([
+
+    fetch(
+      "https://sivaahbackend.onrender.com/api/products"
+    ),
+
+    fetch(
+      "https://sivaahbackend.onrender.com/api/categories"
+    )
+
+  ]);
+
+  const products =
+    await productsRes.json();
+
+  const categories =
+    await categoriesRes.json();
+
+  const sitemap =
+    generateSiteMap({
+
+      products,
+      categories
+    });
+
+  res.setHeader(
+    "Content-Type",
+    "text/xml"
+  );
+
+  res.write(sitemap);
+
+  res.end();
+
+  return {
+    props: {}
+  };
+}
+
+export default function Sitemap() {
+  return null;
+}
