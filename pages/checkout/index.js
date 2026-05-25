bb6bb1d823d9784c01568a2c2a992fa18c07d392
mk2429 <mayank.20429@knit.ac.in>
2026-05-25 17:40:51 +0530

    new c88

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -88,7 +88,8 @@ export default function Checkout() {
       setCouponDiscount(Math.round(totalAmount * 0.10));
       setAppliedCoupon(code);
       return;
-    }if (code === "Nish10") {
+    }
+    if (code === "NISH10") {
       setCouponDiscount(Math.round(totalAmount * 0.10));
       setAppliedCoupon(code);
       return;
